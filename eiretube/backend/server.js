dc4d9100e1f16c86cb1d628a7645cbaa3919e277
osmanseytablaev <osman.seytablaev@gmail.com>
2024-08-07 13:37:57 +0100

    Deploy

--- eiretube/backend/server.js
+++ eiretube/backend/server.js
@@ -20,7 +20,7 @@ app.use(cors({
 
 app.use(express.json());
 
-console.log(`MONGODB_URI: ${process.env.MONGODB_URI}`);
+console.log(`MONGO_URI: ${process.env.MONGO_URI}`);
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
